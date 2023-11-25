21e9f3bca8d5f971627b84e1982afb530e71636f
Jorge Allen <j.allensoler@gmail.com>
2023-11-26 00:31:23 +0100

    cleaned and delete some module.css

--- ymagyn/src/components/Products/Products.js
+++ ymagyn/src/components/Products/Products.js
@@ -3,7 +3,6 @@ import Container from 'react-bootstrap/Container';
 import Row from 'react-bootstrap/Row';
 import Col from 'react-bootstrap/Col';
 
-import "./Products.module.css"
 
 //need to change to an api
 
